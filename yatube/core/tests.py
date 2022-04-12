c6f7ce2c3ebdada391e58e5aa9f6300e563c7bc0
Стас Кучеренко <solomen88@ya.ru>
2022-04-12 21:07:51 +0300

    +++

--- yatube/core/tests.py
+++ yatube/core/tests.py
@@ -2,6 +2,7 @@ from http import HTTPStatus
 
 from django.test import TestCase
 
+
 class ViewTestClass(TestCase):
     def test_error_page(self):
         response = self.client.get('/nonexist-page/')
